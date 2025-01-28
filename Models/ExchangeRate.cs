3e8b1f3ff7dbe84d2c6ad82d7cb281245175f948
KoLiD91 <122565211+KoLiD91@users.noreply.github.com>
2025-01-29 00:06:32 +0100

    Tabela
    Struktura tabeli

---
+++ Models/ExchangeRate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyRates.Models
+{
+    public class ExchangeRate
+    {
+        public int Id { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime Date { get; set; }
+        public string TableType { get; set; }
+        public DateTime FetchDate { get; set; }
+    }
+}
