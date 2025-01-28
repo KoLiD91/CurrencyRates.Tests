1a390178ed550bdeefb58620f5f8caefce14497d
KoLiD91 <122565211+KoLiD91@users.noreply.github.com>
2025-01-29 00:10:04 +0100

    Add files via upload
    połączenie z bazą danych

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyRates.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyRates.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExchangeRate> ExchangeRates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExchangeRate>()
+                .HasIndex(e => new { e.CurrencyCode, e.Date })
+                .IsUnique();
+
+            modelBuilder.Entity<ExchangeRate>()
+                .Property(e => e.Rate)
+                .HasPrecision(18, 4);
+        }
+    }
+}
